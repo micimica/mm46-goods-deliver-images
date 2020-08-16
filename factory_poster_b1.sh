84782ebbbf18c582a22892292c22baa79abd066f
micimica <69741105+micimica@users.noreply.github.com>
2020-08-16 22:59:11 +0900

    Correct generate shell for poster B1, B2

--- factory_poster_b1.sh
+++ factory_poster_b1.sh
@@ -3,22 +3,22 @@
 # Poster - B1 size
 # https://factory.pixiv.net/products/fast_poster
 # -----------------------------
-# image size      : 521 x 734 mm (7180 x 10115 px recommended)
+# image size      : 734 x 1036 mm (10115 x 14276 px recommended)
 # circle size     : 6.80 mm
 # circle count    : 108 (per 734mm)
 # repeatable unit : 94 x 2 = 188 px
 # -----------------------------
 
 # neutral variation
-convert -density 1200 -resize 192x192 -crop 188x188+2+2 resources/mm46_neutral_unit.svg mm46_poster_b2_neutral_unit.png
-convert -append `seq 1 54 | awk '{printf "mm46_poster_b2_neutral_unit.png "}'` mm46_poster_b2_neutral_line.png
-convert +append `seq 1 39 | awk '{printf "mm46_poster_b2_neutral_line.png "}'` mm46_poster_b2_neutral_rect.png
-convert -density 1200 -resize 7305x10115 mm46_poster_b2_neutral_rect.png images-dist/mm46_poster_b2_neutral.png
-rm -f mm46_poster_b2_neutral_*.png
+convert -density 1200 -resize 192x192 -crop 188x188+2+2 resources/mm46_neutral_unit.svg mm46_poster_b1_neutral_unit.png
+convert +append `seq 1 54 | awk '{printf "mm46_poster_b1_neutral_unit.png "}'` mm46_poster_b1_neutral_line.png
+convert -append `seq 1 77 | awk '{printf "mm46_poster_b1_neutral_line.png "}'` mm46_poster_b1_neutral_rect.png
+convert -density 1200 -resize 10115x14423 mm46_poster_b1_neutral_rect.png images-dist/mm46_poster_b1_neutral.png
+rm -f mm46_poster_b1_neutral_*.png
 
 # chromatic variation
-convert -density 1200 -resize 192x192 -crop 188x188+2+2 resources/mm46_chromatic_unit.svg mm46_poster_b2_chromatic_unit.png
-convert -append `seq 1 54 | awk '{printf "mm46_poster_b2_chromatic_unit.png "}'` mm46_poster_b2_chromatic_line.png
-convert +append `seq 1 39 | awk '{printf "mm46_poster_b2_chromatic_line.png "}'` mm46_poster_b2_chromatic_rect.png
-convert -density 1200 -resize 7305x10115 mm46_poster_b2_chromatic_rect.png images-dist/mm46_poster_b2_chromatic.png
-rm -f mm46_poster_b2_chromatic_*.png
+convert -density 1200 -resize 192x192 -crop 188x188+2+2 resources/mm46_chromatic_unit.svg mm46_poster_b1_chromatic_unit.png
+convert +append `seq 1 54 | awk '{printf "mm46_poster_b1_chromatic_unit.png "}'` mm46_poster_b1_chromatic_line.png
+convert -append `seq 1 77 | awk '{printf "mm46_poster_b1_chromatic_line.png "}'` mm46_poster_b1_chromatic_rect.png
+convert -density 1200 -resize 10115x14423 mm46_poster_b1_chromatic_rect.png images-dist/mm46_poster_b1_chromatic.png
+rm -f mm46_poster_b1_chromatic_*.png
