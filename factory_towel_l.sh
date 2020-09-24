0a85ae929f83ef28ea0b4beb2bf3962970c96930
micimica <69741105+micimica@users.noreply.github.com>
2020-09-24 22:23:55 +0900

    support mici-mica version 46-3

--- factory_towel_l.sh
+++ factory_towel_l.sh
@@ -9,8 +9,12 @@
 # repeatable unit : 546 x 2 = 1092 px
 # -----------------------------
 
+UNITSIZE=1092
+TEMPSIZE=$(( $UNITSIZE * 3 ))
+
 # neutral variation
-convert -density 1200 -resize 1100x1100 -crop 1092x1092+4+4 resources/mm46_neutral_unit.svg mm46_towel_l_neutral_unit.png
+convert -density 1200 -resize ${TEMPSIZE}x${TEMPSIZE} -fuzz 20% -trim resources/mm46-3_neutral_unit.svg mm46_towel_l_neutral_unit_temp.png
+convert -density 1200 -resize ${UNITSIZE}x${UNITSIZE} mm46_towel_l_neutral_unit_temp.png mm46_towel_l_neutral_unit.png
 convert +append `seq 1 8 | awk '{printf "mm46_towel_l_neutral_unit.png "}'` mm46_towel_l_neutral_line.png
 convert -append `seq 1 8 | awk '{printf "mm46_towel_l_neutral_line.png "}'` mm46_towel_l_neutral_rect.png
 convert -density 1200 -rotate +30 mm46_towel_l_neutral_rect.png mm46_towel_l_neutral_rot.png
@@ -18,7 +22,8 @@ convert -density 1200 -gravity center -crop 4371x4725-2186-2363 mm46_towel_l_neu
 rm -f mm46_towel_l_neutral_*.png
 
 # chromatic variation
-convert -density 1200 -resize 1100x1100 -crop 1092x1092+4+4 resources/mm46_chromatic_unit.svg mm46_towel_l_chromatic_unit.png
+convert -density 1200 -resize ${TEMPSIZE}x${TEMPSIZE} -fuzz 20% -trim resources/mm46-3_chromatic_unit.svg mm46_towel_l_chromatic_unit_temp.png
+convert -density 1200 -resize ${UNITSIZE}x${UNITSIZE} mm46_towel_l_chromatic_unit_temp.png mm46_towel_l_chromatic_unit.png
 convert +append `seq 1 8 | awk '{printf "mm46_towel_l_chromatic_unit.png "}'` mm46_towel_l_chromatic_line.png
 convert -append `seq 1 8 | awk '{printf "mm46_towel_l_chromatic_line.png "}'` mm46_towel_l_chromatic_rect.png
 convert -density 1200 -rotate +30 mm46_towel_l_chromatic_rect.png mm46_towel_l_chromatic_rot.png
